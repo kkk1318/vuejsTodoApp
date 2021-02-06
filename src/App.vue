5b87a6d05526da1d56d7ac8c1ca8c2c108efac76
kkk1318 <kkk1318@hanmail.net>
2021-02-06 20:43:51 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput v-on:addTodo="addTodo"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" v-on:removeTodo="removeTodo"></TodoList>
+    <TodoFooter v-on:removeAll="clearAll"></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoFooter from './components/TodoFooter.vue'
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+export default {
+  data() {
+    return {
+      todoItems: []  //데이터 속석 todoItems 선언
+    }
+  },
+  methods: {
+    addTodo(todoItem){
+      localStorage.setItem(todoItem, todoItem);
+      this.todoItems.push(todoItem);
+    },
+    clearAll(){
+      localStorage.clear();
+      this.todoItems = [];
+    },
+    removeTodo(todoItem, index){
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    }
+  },
+  components: {
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList,
+    'TodoFooter': TodoFooter
+  },
+  created () {
+        if (localStorage.length > 0) {
+            for (var i = 0;  i<localStorage.length; i++) {
+                this.todoItems.push(localStorage.key(i));
+            }
+
+        }
+    }
+}
+</script>
+
+<style>
+    body {
+        text-align: center;
+        background-color: #F6F6F8;
+    }
+    input{
+        border-style: groove;
+        width: 200px;
+    }
+    button {
+        border-style: groove;
+    }
+    .shadow {
+        box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03)
+    }
+</style>
